b907f620877f462c3413b17c0c52f8b988b07929
kirtusleyba <kleyba@asu.edu>
2025-10-15 19:11:42 -0700

    Added layer for loading node data

--- app/fileLoader.go
+++ app/fileLoader.go
@@ -1,61 +1,202 @@
 package app
 
 import (
-	rl "github.com/gen2brain/raylib-go/raylib"
+	"errors"
 	gui "github.com/gen2brain/raylib-go/raygui"
+	rl "github.com/gen2brain/raylib-go/raylib"
+	"log"
+	"math"
+	"os"
 )
-//
-
-// type Layer interface {
-// 	Attach(*[]Layer)
-// 	Detach()
-// 	OnEvent()
-// 	OnUpdate()
-// 	OnRender()
-// 	SetTransform(origin, size Vec2Df32)
-// }
 
 type FileLoadLayer struct {
-	origin Vec2Df32
-	size Vec2Df32
-	scrollVal int32
-	layerStack *[]Layer
+	origin        Vec2Df32
+	size          Vec2Df32
+	scrollVal     int32
+	layerStack    *[]Layer
+	workingDir    string
+	fileEntries   []os.DirEntry
+	selectedEntry int
+	entrySize     int
+	fileToLoad string
+	callback func(fname string)
 }
 
-func (f *FileLoadLayer) Attach(layerStack *[]Layer){
+func (f *FileLoadLayer) Attach(layerStack *[]Layer) {
 	f.layerStack = layerStack
 	f.scrollVal = 0
+	f.workingDir = "./"
+	f.selectedEntry = 0
+	f.entrySize = 32
 }
 
-func (f *FileLoadLayer) Detach(){}
-func (f *FileLoadLayer) OnEvent(){}
-func (f *FileLoadLayer) OnUpdate(){}
-func (f *FileLoadLayer) OnRender(){
+func (f *FileLoadLayer) Detach()  {}
+func (f *FileLoadLayer) OnEvent() {}
+func (f *FileLoadLayer) OnUpdate() {
+	f.updateFileEntries()
+	f.updateSelectedEntry()
+	err := f.processClicks()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+func (f *FileLoadLayer) OnRender() {
 
 	screenWidth := float32(rl.GetScreenWidth())
 	screenHeight := float32(rl.GetScreenHeight())
-	fileDialogRect := rl.Rectangle{( f.origin.x * screenWidth  ),
-								   ( f.origin.y * screenHeight ),
-								   ( f.size.x   * screenWidth  ),
-								   ( f.size.y   * screenHeight )}
+	f.drawFileDialog(screenWidth, screenHeight)
+
+}
+func (f *FileLoadLayer) SetTransform(origin, size Vec2Df32) {
+	f.origin = origin
+	f.size = size
+}
+func (f *FileLoadLayer) SetCallback(c func(fname string)){
+	f.callback = c
+}
+
+func (f *FileLoadLayer) updateFileEntries() {
+	//get the current dir entries
+	dirEntries, err := os.ReadDir(f.workingDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	f.fileEntries = nil
+	for _, entry := range dirEntries {
+		f.fileEntries = append(f.fileEntries, entry)
+	}
+}
+
+func (f *FileLoadLayer) drawFileDialog(screenWidth, screenHeight float32) {
+	//draw the file dialog
+	fileDialogRect := rl.Rectangle{(f.origin.x * screenWidth),
+		(f.origin.y * screenHeight),
+		(f.size.x * screenWidth),
+		(f.size.y * screenHeight)}
 	fileDialogColor := rl.NewColor(77, 77, 77, 200)
 	rl.DrawRectangle(int32(fileDialogRect.X),
-					 int32(fileDialogRect.Y),
-					 int32(fileDialogRect.Width),
-					 int32(fileDialogRect.Height), fileDialogColor)
+		int32(fileDialogRect.Y),
+		int32(fileDialogRect.Width),
+		int32(fileDialogRect.Height), fileDialogColor)
 
+	//draw the scroll bar
 	var sliderOrigin, sliderSize Vec2Df32
-	sliderOrigin.x =  (f.origin.x*screenWidth)+(f.size.x*screenWidth)
-	sliderOrigin.y  = (f.origin.y*screenHeight)
-	sliderSize.x = 0.02*fileDialogRect.Width
+	sliderOrigin.x = (f.origin.x * screenWidth) + (f.size.x * screenWidth)
+	sliderOrigin.y = (f.origin.y * screenHeight)
+	sliderSize.x = 0.02 * fileDialogRect.Width
 	sliderSize.y = fileDialogRect.Height
 
 	scrollBarBounds := rl.Rectangle{sliderOrigin.x, sliderOrigin.y, sliderSize.x, sliderSize.y}
+	f.scrollVal = gui.ScrollBar(scrollBarBounds, f.scrollVal, 0, int32(len(f.fileEntries)+1))
 
-	f.scrollVal = gui.ScrollBar(scrollBarBounds, f.scrollVal, 0, 100)
+	//draw the selected entry rectangle
+	idx := f.selectedEntry
+	if idx != -1 {
+		selectedEntryColor := rl.NewColor(200, 200, 200, 200)
+		selectedEntryRect := rl.Rectangle{fileDialogRect.X,
+										  fileDialogRect.Y+float32((idx - int(f.scrollVal))*f.entrySize),
+			fileDialogRect.Width, float32(f.entrySize)}
+		rl.DrawRectangle(int32(selectedEntryRect.X),
+			int32(selectedEntryRect.Y),
+			int32(selectedEntryRect.Width),
+			int32(selectedEntryRect.Height),
+			selectedEntryColor)
+	}
 
+	//draw the available files
+	rem := int(fileDialogRect.Height) % f.entrySize
+	minY := int(fileDialogRect.Y)
+	maxY := int(math.Min(float64(minY+f.entrySize*(len(f.fileEntries)+1)),
+		float64(minY+int(fileDialogRect.Height)-rem)))
+	if int(f.scrollVal) == 0 {
+		rl.DrawText("(dir) ../",
+			int32(fileDialogRect.X),
+			int32(fileDialogRect.Y)+int32(f.entrySize*(0-int(f.scrollVal))),
+			int32(f.entrySize), rl.White)
+	}
+	for i, dir_entry := range f.fileEntries {
+		draw_idx := i - int(f.scrollVal)
+		yval := int(int32(fileDialogRect.Y)+int32(f.entrySize*(draw_idx+1))) + f.entrySize
+		if yval > maxY || yval < minY+f.entrySize {
+			continue
+		}
+		var name string = dir_entry.Name()
+		if dir_entry.IsDir() {
+			name = "(dir) " + name
+		}
+		rl.DrawText(name,
+			int32(fileDialogRect.X),
+			int32(fileDialogRect.Y)+int32(f.entrySize*(draw_idx+1)),
+			int32(f.entrySize), rl.White)
+	}
 }
-func (f* FileLoadLayer) SetTransform(origin, size Vec2Df32){
-	f.origin = origin
-	f.size = size
+
+func (f *FileLoadLayer) updateSelectedEntry() {
+	var mouseVec rl.Vector2 = rl.GetMousePosition()
+
+	screenWidth := float32(rl.GetScreenWidth())
+	screenHeight := float32(rl.GetScreenHeight())
+
+	fileDialogRect := rl.Rectangle{(f.origin.x * screenWidth),
+		(f.origin.y * screenHeight),
+		(f.size.x * screenWidth),
+		(f.size.y * screenHeight)}
+
+	minY := int(fileDialogRect.Y)
+	rem := int(fileDialogRect.Height) % f.entrySize
+	maxY := int(math.Min(float64(minY+f.entrySize*(len(f.fileEntries)+1)),
+		float64(minY+int(fileDialogRect.Height)-rem)))
+	actualY := int(mouseVec.Y)
+	inDialogY := mouseVec.Y - fileDialogRect.Y
+
+	minX := int(fileDialogRect.X)
+	maxX := minX + int(fileDialogRect.Width)
+	actualX := int(mouseVec.X)
+	if actualX < minX || actualX > maxX {
+		f.selectedEntry = -1
+		return
+	}
+
+	if actualY >= maxY {
+		f.selectedEntry = -1
+	} else if actualY < minY {
+		f.selectedEntry = -1
+	} else {
+		idx := int(f.scrollVal) + (int(inDialogY) / f.entrySize)
+		if(idx <= len(f.fileEntries)){
+			f.selectedEntry = int(f.scrollVal) + (int(inDialogY) / f.entrySize)
+		} else {
+			f.selectedEntry = -1
+		}
+	}
+}
+
+func (f *FileLoadLayer) processClicks() error {
+	if rl.IsMouseButtonReleased(0) {
+		if f.selectedEntry > len(f.fileEntries) {
+			return errors.New("selected entry idx is greater than the number of entries")
+		}
+		if f.selectedEntry == -1 {
+			return nil
+		}
+
+		if f.selectedEntry > 0 {
+			var idx int = f.selectedEntry - 1
+			entry := f.fileEntries[idx]
+			if entry.IsDir() {
+				f.workingDir = f.workingDir + "/" + entry.Name()
+				f.scrollVal = 0
+			} else {
+				//try to load this file
+				f.fileToLoad = f.workingDir + "/" + entry.Name()
+				PopLayer(f.layerStack)
+				f.callback(f.fileToLoad)
+			}
+		} else {
+			f.workingDir = f.workingDir + "/../"
+			f.scrollVal = 0
+		}
+	}
+
+	return nil
 }
--- app/ui.go
+++ app/ui.go
@@ -1,9 +1,15 @@
 package app
 
 import (
-	rl "github.com/gen2brain/raylib-go/raylib"
-	gui "github.com/gen2brain/raylib-go/raygui"
+	"encoding/csv"
+	"os"
+	"log"
+	"fmt"
 	"strconv"
+	"strings"
+
+	gui "github.com/gen2brain/raylib-go/raygui"
+	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
 type UIState int
@@ -33,10 +39,17 @@ func (u *UILayer) OnUpdate() {
 }
 func (u *UILayer) OnRender() {
 	u.drawStats()
-	loadFilePressed := u.drawButtons()
-	if(loadFilePressed && u.currentState == UIMain){
+	loadNodeFile := u.drawNodeButton()
+	if(loadNodeFile && u.currentState == UIMain){
 		var fileLoadLayer FileLoadLayer
 		fileLoadLayer.SetTransform(Vec2Df32{0.1, 0.1}, Vec2Df32{0.8, 0.8})
+
+		loadCallback := func(fname string){
+			u.currentState = UIMain
+			u.loadNodeData(fname)
+		}
+
+		fileLoadLayer.SetCallback(loadCallback)
 		PushLayer(u.layerStack, &fileLoadLayer)
 		u.currentState = UILoad
 	}
@@ -60,7 +73,7 @@ func (u *UILayer) drawStats(){
 	rl.DrawText("FPS: " + fpsStr, int32(infoBoxOrigin.x + 8), int32(infoBoxOrigin.y + 8), 16, rl.White)
 }
 
-func (u *UILayer) drawButtons() bool {
+func (u *UILayer) drawNodeButton() bool {
 	screenWidth := float32(rl.GetScreenWidth())
 	screenHeight := float32(rl.GetScreenHeight())
 
@@ -76,11 +89,26 @@ func (u *UILayer) drawButtons() bool {
 	buttonSize := Vec2Df32{  x : 0.8*infoBoxSize.x,
 							 y : 0.05*infoBoxSize.y}
 
-	loadFilePressed := gui.Button( rl.Rectangle{buttonOrigin.x, buttonOrigin.y, buttonSize.x, buttonSize.y}, "Load Network")
+	loadFilePressed := gui.Button( rl.Rectangle{buttonOrigin.x, buttonOrigin.y, buttonSize.x, buttonSize.y}, "Load Node Data")
 	return loadFilePressed
 }
 
 func (u *UILayer) SetTransform(origin, size Vec2Df32){
 	u.origin = origin
 	u.size = size
+}
+
+func (u *UILayer) loadNodeData(fname string){
+	content, err := os.ReadFile(fname)
+	if(err != nil){
+		log.Fatal(err)
+	}
+	csvReader := csv.NewReader(strings.NewReader(string(content)))
+	records, err := csvReader.ReadAll()
+	if(err != nil){
+		log.Fatal(err)
+	}
+	for _, record := range records {
+		fmt.Printf("%v\n", record)
+	}
 }
